4bc30da8672c4700d8eabaafd0eda5e2aefecec3
dayan iskhakov <dayan.iskhakov@gmail.com>
2025-07-20 23:46:04 +0300

    added type hinting

--- task_tracker.py
+++ task_tracker.py
@@ -5,26 +5,33 @@ import os
 import sys
 
 
-def _read_json():
+class CommandNotFoundError(Exception):
+    '''Исключение, вызываемое при вводе несуществующей команды.'''
+    def __init__(self, command: str):
+        self.command = command
+        super().__init__(f"Команда '{command}' не существует.")
+
+
+def _read_json() -> dict[str: list, str: int]:
     with open('tasks.json', 'r', encoding='utf-8') as file:
         json_data = json.load(file)
     return json_data
 
 
-def _write_json(python_data):
+def _write_json(python_data: dict[str: list, str: int]) -> None:
     with open('tasks.json', 'w', encoding='utf-8') as file:
         json.dump(python_data, file)
 
 
-def _now_datetime():
+def _now_datetime() -> str:
     return datetime.datetime.now().strftime('%d.%m.%Y %H:%M')
 
 
-def _add(json_data, description):
+def _add(json_data: dict[str: list, str: int], description: str) -> None:
     json_data['curr_id'] += 1
     task = {
         'id': json_data['curr_id'],
-        'description': description,
+        'description': description.strip(),
         'status': 'todo',
         'created': _now_datetime(),
         'updated': _now_datetime()
@@ -32,35 +39,49 @@ def _add(json_data, description):
     json_data['tasks'].append(task)
 
 
-def _update(json_data, id, description):
+def _update(
+    json_data: dict[str: list, str: int],
+    id: int,
+    description: str) -> None:
     for i in range(len(json_data['tasks'])):
         if json_data['tasks'][i]['id'] == int(id):
             json_data['tasks'][i]['updated'] = _now_datetime()
-            json_data['tasks'][i]['description'] = description
+            json_data['tasks'][i]['description'] = description.strip()
+            break
+    else:
+        raise IndexError
 
 
-def _delete(json_data, id):
+def _delete(json_data: dict[str: list, str: int], id: int) -> None:
     for i in range(len(json_data['tasks'])):
         if json_data['tasks'][i]['id'] == int(id):
             json_data['tasks'].pop(i)
             break
+    else:
+        raise IndexError
 
 
-def _mark_in_progress(json_data, id):
+def _mark_in_progress(json_data: dict[str: list, str: int], id: int) -> None:
     for i in range(len(json_data['tasks'])):
         if json_data['tasks'][i]['id'] == int(id):
             json_data['tasks'][i]['status'] = 'in-progress'
             json_data['tasks'][i]['updated'] = _now_datetime()
+            break
+    else:
+        raise IndexError
 
 
-def _mark_done(json_data, id):
+def _mark_done(json_data: dict[str: list, str: int], id: id) -> None:
     for i in range(len(json_data['tasks'])):
         if json_data['tasks'][i]['id'] == int(id):
             json_data['tasks'][i]['status'] = 'done'
             json_data['tasks'][i]['updated'] = _now_datetime()
+            break
+    else:
+        raise IndexError
 
 
-def _list(json_data, _, status=None):
+def _list(json_data: dict[str: list, str: int], _, status=None) -> None:
     print(
         'id',
         'description'.rjust(30),
@@ -81,13 +102,32 @@ def _list(json_data, _, status=None):
                 sep=' | ')
 
 
-def _check_json():
+def _help() -> None:
+    print(
+        'add               command add new task. includes one positional argument - description',
+        '                  add "go to job"\n',
+        'update            command update description of task. includes two positional args: id, description',
+        '                  update 1 "got to job and gym"\n',
+        'delete            command delete task. includes one positional arg - id',
+        '                  delete 1\n',
+        'mark-done         command mark status of task to done. includes one positional arg - id',
+        '                  mark-done 1\n',
+        'mark-in-progress  command mark status of task to in-progress. includes one positional arg - id',
+        '                  mark-in-progress 1\n',
+        'list              command print tasks. includes zero or one positional arg - done/in-progress/todo',
+        '                  list - print all tasks',
+        '                  list todo - print tasks with "todo" status\n',
+        sep='\n'
+    )
+
+
+def _check_json() -> None:
     if 'tasks.json' not in os.listdir():
         tasks = {"tasks": [], "curr_id": 0}
         _write_json(tasks)
 
 
-def _run_cmd(cmd: list):
+def _run_cmd(cmd: list) -> None:
     json_data = _read_json()
 
     match cmd[0]:
@@ -104,16 +144,23 @@ def _run_cmd(cmd: list):
         case 'list':
             _list(json_data, *cmd)
             return
+        case 'help':
+            _help()
+            return
         case _:
-            raise NotImplemented
+            raise CommandNotFoundError(cmd[0])
 
     _write_json(json_data)
 
 
-def main():
+def main() -> None:
     _check_json()
-    _run_cmd(sys.argv[1:])
-
-
-if __name__ == "__main__":
-    main()
+    
+    try:
+        _run_cmd(sys.argv[1:])
+    except CommandNotFoundError as err:
+        print(err)
+    except IndexError as err:
+        print("Нет задачи с таким id!")
+    except ValueError as err:
+        print('id must be number')
